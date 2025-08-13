42a0e0b9b15005b73e5f3b5ad7a05a5f156b064e
Ismail Taher <ismail.taher@ymail.com>
2025-08-13 22:23:05 +0500

    add knexfile.js and db/db.js

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+// Update with your config settings.
+require('dotenv').config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    searchPath: ['knex', 'public'],
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: { tableName: 'knex_migrations' },
+    seeds: { directory: './db/seeds' },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    searchPath: ['knex', 'public'],
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+    seeds: { directory: './db/seeds' },
+  },
+};
